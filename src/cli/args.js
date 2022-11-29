a4a1f4f63bb0835ad67f750f4383476faf622d5c
aless133 <aless133@yandex.ru>
2022-11-30 00:55:31 +0300

    fix: args separator

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,7 @@ const parseArgs = () => {
     const args=[]
     for (let i=2;i<process.argv.length;i+=2)
         args.push(process.argv[i].substring(2)+' is '+process.argv[i+1]);
-    console.log(args.join("; "));
+    console.log(args.join(", "));
 };
 
 parseArgs();
