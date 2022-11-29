8281c76742891d302c16b69c3f2a4970d8388a60
aless133 <aless133@yandex.ru>
2022-11-29 10:54:44 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fsPromises from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  const filename="./src/fs/files/fresh.txt";
+  let error = false;
+  try {
+    await fsPromises.access(filename);
+    error = true;
+  } catch (e) {
+    await fsPromises.writeFile(filename,"I am fresh and young");
+  }
+  if (error) {
+    throw Error('FS operation failed');
+  }
 };
 
 await create();
