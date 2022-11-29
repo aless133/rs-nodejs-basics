7566947ee0955cb86a54ffad952f8b01cc634b79
aless133 <aless133@yandex.ru>
2022-11-30 00:55:01 +0300

    feat: implement cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rss=[];
+    for (let key in process.env) {
+        if (key.substring(0,4)=="RSS_")
+            rss.push(key+"="+process.env[key]);
+    }
+    console.log(rss.join("; "));
 };
 
 parseEnv();
