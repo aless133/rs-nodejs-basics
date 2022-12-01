13f8b0e816fd9a94304c6589c44ddd7a14adbb60
aless133 <aless133@yandex.ru>
2022-12-01 19:23:17 +0300

    feat: implement fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import fsPromises from 'node:fs/promises';
+import { found, __dirname } from './common.js';
+
 const read = async () => {
-    // Write your code here 
+    const filename = __dirname+"/files/fileToRead.txt";
+    if ( await found(filename) ) {
+        console.log(await fsPromises.readFile(filename, {encoding:'utf8'}));
+    } else {
+        throw Error ('FS operation failed');
+    }
 };
 
 await read();
