befc1daa1d36a1232861fe9919aa3683757bb9bd
aless133 <aless133@yandex.ru>
2022-12-03 01:09:13 +0300

    feat: implement zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import zlib from "node:zlib";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream";
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = zlib.createGzip();
+    const inp = createReadStream(__dirname + "/files/fileToCompress.txt");
+    const out = createWriteStream(__dirname + "/files/archive.gz");
+    pipeline(inp, gzip, out, (err) => {
+        if (err) {
+            console.error("An error occurred:", err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
