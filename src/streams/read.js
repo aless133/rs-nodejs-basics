3cd3658c09ded53856f5ecf3ed8fb21ca048306b
aless133 <aless133@yandex.ru>
2022-12-03 00:37:45 +0300

    feat: implement streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import { createReadStream } from "fs";
+import { pipeline } from "stream";
+
 const read = async () => {
-    // Write your code here 
+    pipeline(
+        createReadStream(__dirname + "/files/fileToRead.txt"),
+        process.stdout,
+        (err) => {
+            if (err) {
+                console.error("Pipeline failed.", err);
+            }
+        }
+    );
 };
 
 await read();
