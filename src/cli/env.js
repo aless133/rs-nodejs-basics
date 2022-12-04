8aa7698149f5f8f82159ddbe67485779843df026
aless133 <aless133@yandex.ru>
2022-12-04 15:49:36 +0300

    fix: use === instead of ==

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,7 +1,7 @@
 const parseEnv = () => {
     const rss=[];
     for (let key in process.env) {
-        if (key.substring(0,4)=="RSS_")
+        if (key.substring(0,4)==="RSS_")
             rss.push(key+"="+process.env[key]);
     }
     console.log(rss.join("; "));
